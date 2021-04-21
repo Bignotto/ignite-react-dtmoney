5ee1a1968a8112d27ce2152982d7cf7ec23f0632
Thiago Bignotto <bignotto@gmail.com>
2021-04-21 07:40:01 -0300

    Styled modal form

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,4 +1,7 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
+
+import closeImg from "../../assets/close.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -9,8 +12,27 @@ export function NewtransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>this is the modal window!</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar nova transação" />
+      </button>
+
+      <Container>
+        <h2>Nova transação:</h2>
+        <input placeholder="Descrição" />
+        <input type="number" placeholder="Valor" />
+        <input placeholder="Categoria" />
+        <button>Salvar</button>
+      </Container>
     </Modal>
   );
 }
